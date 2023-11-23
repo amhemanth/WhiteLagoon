d35903352f174d2842795d744145ef335de0a6ed
AM_Hemanth <amhemanth1999@gmail.com>
2023-11-23 06:44:06 +0530

    Initial Commit

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _dBContext;
+        public VillaController(ApplicationDbContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+        public IActionResult Index()
+        {
+            var villas = _dBContext.Villas.ToList();
+            return View();
+        }
+    }
+}
